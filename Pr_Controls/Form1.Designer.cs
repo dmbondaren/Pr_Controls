780a386d84d092a02bb2078c48dadfd4ebdcd4e0
Dmytro Bondarenko <Dmytro.Bondarenko@cs.khpi.edu.ua>
2024-05-26 13:52:52 +0300

    Add project files.

---
+++ Pr_Controls/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Pr_Controls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(360, 232);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(233, 143);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(29, 232);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(257, 143);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(302, 427);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 2;
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(302, 468);
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 412);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 131);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fontDialog1
+            // 
+            this.fontDialog1.Apply += new System.EventHandler(this.fontDialog1_Apply);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(925, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(338, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(341, 154);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 8;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 555);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ Pr_Controls/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Pr_Controls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTreeView();
+            InitializeListView();
+            InitializeContextMenu();
+            InitializeMenu();
+        }
+
+        private void InitializeTreeView()
+        {
+            TreeNode semester1 = new TreeNode("Semester 1");
+            semester1.Nodes.Add("Mathematics");
+            semester1.Nodes.Add("Physics");
+            semester1.Nodes.Add("Chemistry");
+
+            TreeNode semester2 = new TreeNode("Semester 2");
+            semester2.Nodes.Add("Biology");
+            semester2.Nodes.Add("History");
+            semester2.Nodes.Add("Computer Science");
+
+            treeView1.Nodes.Add(semester1);
+            treeView1.Nodes.Add(semester2);
+        }
+
+        private void InitializeListView()
+        {
+            listView1.View = View.Details;
+            listView1.Columns.Add("Group Number", 150);
+            listView1.Columns.Add("Student Name", 150);
+            listView1.FullRowSelect = true;
+            listView1.GridLines = true;
+        }
+
+        private void InitializeContextMenu()
+        {
+            // Добавляем пункты меню в contextMenuStrip1
+            ToolStripMenuItem colorMenuItem = new ToolStripMenuItem("Цвет текста");
+            ToolStripMenuItem fontMenuItem = new ToolStripMenuItem("Шрифт");
+
+            // Привязываем обработчики событий
+            colorMenuItem.Click += new EventHandler(цветТекстаToolStripMenuItem_Click);
+            fontMenuItem.Click += new EventHandler(шрифтToolStripMenuItem_Click);
+
+            // Добавляем пункты меню в contextMenuStrip1
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { colorMenuItem, fontMenuItem });
+
+            // Привязываем contextMenuStrip1 к label1
+            label1.ContextMenuStrip = contextMenuStrip1;
+        }
+
+        private void InitializeMenu()
+        {
+            // Создаем меню
+            ToolStripMenuItem fileMenu = new ToolStripMenuItem("Файл");
+            ToolStripMenuItem exitMenuItem = new ToolStripMenuItem("Выход");
+            exitMenuItem.Click += new EventHandler(выходToolStripMenuItem_Click);
+            fileMenu.DropDownItems.Add(exitMenuItem);
+
+            ToolStripMenuItem dateMenu = new ToolStripMenuItem("Работа с датами");
+            ToolStripMenuItem clockMenuItem = new ToolStripMenuItem("Часы");
+            clockMenuItem.Click += new EventHandler(часыToolStripMenuItem_Click);
+            ToolStripMenuItem remainingTimeMenuItem = new ToolStripMenuItem("Расчет остатка времени");
+            remainingTimeMenuItem.Click += new EventHandler(расчетостаткаВремениToolStripMenuItem_Click);
+            dateMenu.DropDownItems.Add(clockMenuItem);
+            dateMenu.DropDownItems.Add(remainingTimeMenuItem);
+
+            // Добавляем пункты меню в menuStrip1
+            menuStrip1.Items.Add(fileMenu);
+            menuStrip1.Items.Add(dateMenu);
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            MessageBox.Show("You selected: " + e.Node.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string groupNumber = maskedTextBox1.Text.Trim();
+            string studentName = maskedTextBox2.Text.Trim();
+
+            if (!string.IsNullOrEmpty(groupNumber) && !string.IsNullOrEmpty(studentName))
+            {
+                ListViewGroup group = null;
+                foreach (ListViewGroup g in listView1.Groups)
+                {
+                    if (g.Header == groupNumber)
+                    {
+                        group = g;
+                        break;
+                    }
+                }
+
+                if (group == null)
+                {
+                    group = new ListViewGroup(groupNumber, groupNumber);
+                    listView1.Groups.Add(group);
+                }
+
+                ListViewItem item = new ListViewItem(new string[] { groupNumber, studentName });
+                item.Group = group;
+                listView1.Items.Add(item);
+
+                maskedTextBox1.Clear();
+                maskedTextBox2.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter both group number and student name.");
+            }
+        }
+
+        // Обработчик для пункта "Цвет текста" в контекстном меню
+        private void цветТекстаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                label1.ForeColor = colorDialog1.Color;
+            }
+        }
+
+        // Обработчик для пункта "Шрифт" в контекстном меню
+        private void шрифтToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                label1.Font = fontDialog1.Font;
+            }
+        }
+
+        private void fontDialog1_Apply(object sender, EventArgs e)
+        {
+            // Optional: Add code here if needed for the Apply event of fontDialog1
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+            // Optional: Add code here if needed for the Popup event of toolTip1
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // Optional: Add code here if needed for the Click event of label1
+        }
+
+        private void contextMenuStrip1_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // Optional: Add code here if needed for the Opening event of contextMenuStrip1
+        }
+
+        // Обработчик для пункта "Выход" в меню
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        // Обработчик для пункта "Часы" в меню
+        private void часыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+            timer1.Tick += delegate {
+                label2.Text = DateTime.Now.TimeOfDay.ToString() + " далее время по частям: " +
+                              DateTime.Now.Hour + "-" + DateTime.Now.Minute + "-" + DateTime.Now.Second.ToString();
+            };
+        }
+
+        // Обработчик для пункта "Расчет остатка времени" в меню
+        private void расчетостаткаВремениToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            DateTime dt1 = dateTimePicker1.Value; // выбранное время
+            label2.Text = "до выбранной даты осталось дней: " + (dt1.DayOfYear - DateTime.Now.DayOfYear).ToString();
+        }
+
+        // Недостающие обработчики
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            // Ваш код для обработки изменения даты здесь
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            // Ваш код для обработки клика по метке здесь
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            // Ваш код для обработки клика по пункту меню здесь
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Ваш код для обработки тика таймера здесь
+        }
+    }
+}
